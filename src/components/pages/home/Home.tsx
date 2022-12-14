5b67ec14c53c83ad276edc7ddcbb85f8b643e295
shiropt <shiro19880610@yahoo.co.jp>
2022-12-15 00:04:27 +0900

    パネルレイアウトを修正

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -13,7 +13,6 @@ export const Home: FC = () => {
       <div className="">
         <div className="flex justify-between mb-8">
           <div className="flex flex-col justify-between w-2/4">
-            <TablePanel />
             <ChartPanel />
           </div>
 
@@ -21,6 +20,7 @@ export const Home: FC = () => {
             <WithTitlePanel title="入力">
               <MoneyDiaryForm />
             </WithTitlePanel>
+            <TablePanel />
           </div>
         </div>
         <WithTitlePanel title="収支一覧">
