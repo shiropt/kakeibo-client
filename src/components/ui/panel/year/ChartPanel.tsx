73986c90ce973c5dfa6083d82dac193728bff372
shiropt <shiro19880610@yahoo.co.jp>
2022-12-29 12:43:40 +0900

    年間収支推移グラフ作成

--- src/components/ui/panel/year/ChartPanel.tsx
+++ src/components/ui/panel/year/ChartPanel.tsx
@@ -1,44 +1,55 @@
 import { FC } from "react";
 import { WithTitlePanel } from "../WithTitlePanel";
-import { AggregateTable } from "../../table/AggregateTable";
 import { useMoneyDiary } from "../../../../hooks/useMoneyDiary";
-import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { Box } from "@mantine/core";
+import { store } from "../../../../libs/store";
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 export const ChartPanel: FC = () => {
-  const { moneyDiaries } = useMoneyDiary();
+  const { year } = store.search();
 
-  if (!moneyDiaries) return <WithTitlePanel title="支出推移">{null}</WithTitlePanel>;
+  const { aggregates } = useMoneyDiary();
+  if (!aggregates) return <p>...</p>;
+  const result = aggregates.aggregateByMonth.filter((aggregate) => aggregate.date === year);
+  const targets = aggregates.aggregateByMonth.filter((month) => month.date.slice(0, 4) === year);
+  let total = 0;
 
-  const data = moneyDiaries
-    .filter((moneyDiary) => moneyDiary.payment > 0)
-    .map((moneyDiary) => {
-      return { name: moneyDiary.expenseItemName, value: moneyDiary.payment };
-    })
-    .sort((a, b) => b.value - a.value);
-  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
-  const renderCustomizedLabel = ({ name, value }: any) => {
-    return `${name} ${value.toLocaleString()}円`;
+  const totals = targets.map((target, i) => {
+    total = total + target.incomeAndExpenditure;
+    return total;
+  });
+
+  const labels = targets.map((month) => month.date);
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "収支",
+        data: totals,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+        yAxisID: "y",
+      },
+    ],
   };
+  console.log({ result });
 
   return (
-    <WithTitlePanel title="出費内訳">
-      <PieChart width={700} height={190}>
-        <Pie
-          data={data}
-          labelLine={false}
-          label={renderCustomizedLabel}
-          outerRadius={60}
-          fill="#8884d8"
-          nameKey="name"
-          dataKey="value"
-          cx="45%"
-          isAnimationActive={false}
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-      </PieChart>
+    <WithTitlePanel title="年間収支推移">
+      <Box w={700}>
+        <Line data={data} />
+      </Box>
     </WithTitlePanel>
   );
 };
